2f7032932d700c6e52956882b78f5de58e14d634
MaexBower <max@maexbower.de>
2017-04-23 16:06:21 +0200

    update jquery, update materializecss, add jqplot to show history of data, bugfixes

--- webUI/remote.php
+++ webUI/remote.php
@@ -1,9 +1,9 @@
 <?php
 //print_r($_GET);
 //print_r($_POST);
-if(isset($_GET["action"]))
+if(isset($_REQUEST["action"]))
 {
-	$action = htmlspecialchars($_GET['action']);
+	$action = htmlspecialchars($_REQUEST['action']);
 }else{
 	printError(2, "no action given.");
 	die;
@@ -15,6 +15,15 @@ switch ($action) {
 	case "current":
 		printData(getCurrentVal());
 		break;
+    case "history":
+        if(isset($_REQUEST["sensor"]))
+        {
+            printData(getHistory(htmlspecialchars($_REQUEST['sensor'])));
+        }else{
+            printError(2, "no sensor given.");
+            die;
+        }
+        break;
 	default:
 		printError(99, "no valid action");
 		break;
@@ -57,7 +66,29 @@ function getCurrentVal() {
 		"comment" => $data[0]['comment'],
 		"messures" => $messure,
 	);
-	printData(json_encode($array));
+	return json_encode($array);
+}
+function getHistory($sensor_shortname) {
+    $query = "SELECT messungen.id, messungen.timestamp, messungen.comment, data.value, sensor.name AS sensor, sensor.type, sensor.shortname FROM messungen INNER JOIN data ON messungen.id = data.fk_messungen INNER JOIN sensor ON data.fk_sensor = sensor.id WHERE shortname = \"${sensor_shortname}\" ORDER BY messungen.id DESC LIMIT 200";
+    //print_r($query);
+    $data = sql_query($query);
+    //print_r($data);
+    //print_r($row);
+    $i = 0;
+    foreach($data as $row)
+    {
+        $messure[$i]['timestamp'] = $row['timestamp'];
+        $messure[$i]['value'] = $row['value'];
+        //$messure[$i]['comment'] = $row['comment'];
+        $i++;
+    };
+    $array = array(
+        "sensor" => $data[0]['sensor'],
+        "type" => $data[0]['type'],
+        "shortname" => $data[0]['shortname'],
+        "messures" => $messure,
+    );
+    return json_encode($array);
 }
 function connectSQLite($path)
 {
